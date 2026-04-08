b23920ccb5f561d300a0a58025f14daa6c8ee3a7
raze.me <ac.otherside@gmail.com>
2026-04-08 18:59:53 +0530

    making the Home Screen and the front app root, product model also adding the logic for search bar, drawer and categories filter

--- lib/main.dart
+++ lib/main.dart
@@ -28,3 +28,71 @@ void main() async{
   );
   runApp(const BabalonApp());
 }
+
+class BabalonApp extends StatelessWidget{
+  const BablonApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Babalon',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF2F6D0),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFF4A442D),
+          onPrimary: Colors.white,
+          secondary: Color(0xFF3D3522),
+          onSecondary: Color.white,
+          surface: Colors.white,
+          onSurface: Color(0xFF141301),
+        ),
+      ),
+      home: const AuthChecker(),
+    );
+  }
+}
+
+class Product{
+  final String name;
+  final String price;
+  final String imageUrl;
+  final String category;
+  final String unit;
+  final String description;
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.imageUrl,
+    required this.category,
+    this.unit = '',
+    this.description = '',
+  });
+}
+
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  final List<String> categories = ['All', 'Cement', 'Steel', 'Bricks', 'Sand'];
+  int selectedCategoryIndex = 0;
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text('Babalon')),
+      body: Column(
+        children:[
+          _buildSearchBar(),
+          _buildCategories(),
+          Expand(child: _buildProductGrid()),
+        ],
+      ),
+    );
+  }
+}
